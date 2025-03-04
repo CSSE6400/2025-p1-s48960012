5137fa9f051eacd680c3c0f45d439e4bde64ea06
Nitish Madhavan <s4896001@student.uq.edu.au>
2025-03-04 19:44:12 +1000

    Prac 2

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask 
+import datetime
+from .models import db
  
 def create_app(): 
     app = Flask(__name__) 
- 
-    from .views.routes import api 
-    app.register_blueprint(api) 
- 
+     
+    app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///db.sqlite"
+    
+    #Load themodels.
+    from todo.models import db
+    from todo.models.todo import Todo
+    db.init_app(app)
+    
+    #Create thedatabasetables.
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    #Register the blueprints.
+    from todo.views.routes import api
+    app.register_blueprint(api)
+    
     return app
